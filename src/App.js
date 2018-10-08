5468d4bfdcea040cf80514436f98ad1df2ea11c8
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-08 11:12:08 +0100

    adding scrollbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component{
@@ -38,7 +39,9 @@ class App extends Component{
                 <div className='tc'>
                     <h1 className='f1'>Robo Search</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots ={filtredRobots}/>
+                    <Scroll>
+                        <CardList robots ={filtredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
