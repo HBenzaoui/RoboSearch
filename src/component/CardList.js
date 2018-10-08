7cbee2f425a035bb1b2a1a19a84fb698828460ea
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-08 18:30:16 +0100

    fixing bugs

--- src/component/CardList.js
+++ src/component/CardList.js
@@ -3,9 +3,6 @@ import Card  from './Card';
 
 
 const CardList = ({ robots }) => {
-    if(true){
-        throw new Error('Nooo');
-    }
     const cardComponent = robots.map((user,i) =>{
         return (<Card 
                 key = {i}
