ea327ee9745db6f4eb43e93921f1f89c3acf5696
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-08 10:47:14 +0100

    fetching data from jsonplaceholder.typecode.com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component{
@@ -13,21 +12,36 @@ class App extends Component{
         }
     }
 
+    componentDidMount(){
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+
+        
+    }
+
     onSearchChange = (event) =>{
         this.setState({searchField: event.target.value})        
     }
 
+    
     render(){
         const filtredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
-        return(
-            <div className='tc'>
-                <h1 className='f1'>Robo Search</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots ={filtredRobots}/>
-            </div>
-        );
+
+    if (this.state.robots.length === 0){
+        return <h1>Loading</h1>
+    } else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>Robo Search</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots ={filtredRobots}/>
+                </div>
+            );
+        }
     }
     
 }
