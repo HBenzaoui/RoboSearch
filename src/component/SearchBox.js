c84e08d5fb8d29a08dd5d17738bd1b76cf2d6b54
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-12 04:57:02 +0100

    just a smal change on searchbar make it round with a small blue shadow

--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../containers/App.css'; 
 
 const SearchBox = ({searchField,searchChange}) => {
     return (
         <div className='ph6'>
             <input
-                className=' tc input-reset ba b--light-gray bg-light-gray pa3 mb2 db w-100 black-60'
+                className='tc input-reset ba b--light-gray bg-light-gray pa3 mb2 db w-100 black-60 br-pill boutline'
                 type='search'
                 placeholder='Enter your Robot name'
                 onChange={searchChange}
